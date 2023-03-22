7e678eafeab14e9b64a426ce95e2a139ac0196b5
Deepanorangesteps <128530870+Deepanorangesteps@users.noreply.github.com>
2023-03-22 12:29:53 +0530

    changed in gits

--- Add.java
+++ Add.java
@@ -1,7 +1,9 @@
 public class Add {
     public static void main(String[] args) {
-        int a= 3;
+        int a= 10;
         int b = 4;
+        int c = a+b;
+         System.out.println(c);
         System.out.println(a+b);
     }
 }
